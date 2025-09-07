cfdf4b04cc704ce5db36928026921ddf8971473b
monjara <yaginumtt@gmail.com>
2025-09-07 10:41:08 +0900

    rename

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("cargo:rustc-link-lib=framework=ApplicationServices");
     println!("cargo:rustc-link-lib=framework=Carbon");
-    
+
     cc::Build::new()
         .file("src/ax_constants.c")
         .compile("ax_constants");
