ada412dabfd0ed33241182507d96a987b8f46298
monjara <yaginumtt@gmail.com>
2025-09-29 21:28:01 +0900

    refactor

--- crates/wnm/src/main.rs
+++ crates/wnm/src/main.rs
@@ -1,10 +1,10 @@
-use std::thread;
+use std::{collections::HashMap, sync::Arc, thread};
 
 use global_hotkey::{
-    hotkey::{Code, HotKey, Modifiers},
     GlobalHotKeyEvent, GlobalHotKeyManager,
+    hotkey::{Code, HotKey, Modifiers},
 };
-use objc2::{sel, MainThreadMarker, MainThreadOnly};
+use objc2::{MainThreadMarker, MainThreadOnly, sel};
 use objc2_app_kit::{
     NSApplication, NSApplicationActivationPolicy, NSMenu, NSMenuItem, NSStatusBar,
 };
@@ -24,36 +24,26 @@ fn main() {
     let d_u = HotKey::new(Some(Modifiers::META | Modifiers::CONTROL), Code::KeyK);
     let l_r = HotKey::new(Some(Modifiers::META | Modifiers::CONTROL), Code::KeyL);
 
-    let l_l_id = l_l.id();
-    let d_d_id = d_d.id();
-    let u_u_id = u_u.id();
-    let r_r_id = r_r.id();
-    let r_l_id = r_l.id();
-    let u_d_id = u_d.id();
-    let d_u_id = d_u.id();
-    let l_r_id = l_r.id();
+    type Handler = Box<dyn Fn() + Send + Sync + 'static>;
+
+    let handlers: Arc<HashMap<u32, Handler>> = Arc::new(HashMap::from([
+        (l_l.id(), Box::new(|| println!("left left")) as Handler),
+        (d_d.id(), Box::new(|| println!("down down")) as Handler),
+        (u_u.id(), Box::new(|| println!("up up")) as Handler),
+        (r_r.id(), Box::new(|| println!("right right")) as Handler),
+        (r_l.id(), Box::new(|| println!("right left")) as Handler),
+        (u_d.id(), Box::new(|| println!("up down")) as Handler),
+        (d_u.id(), Box::new(|| println!("down up")) as Handler),
+        (l_r.id(), Box::new(|| println!("left right")) as Handler),
+    ]));
 
     thread::spawn(move || {
         let rx = GlobalHotKeyEvent::receiver();
         for ev in rx.iter() {
-            if ev.state == global_hotkey::HotKeyState::Pressed {
-                if ev.id == l_l_id {
-                    println!("left left");
-                } else if ev.id == d_d_id {
-                    println!("down down");
-                } else if ev.id == u_u_id {
-                    println!("up up");
-                } else if ev.id == r_r_id {
-                    println!("right right");
-                } else if ev.id == r_l_id {
-                    println!("right left");
-                } else if ev.id == u_d_id {
-                    println!("up down");
-                } else if ev.id == d_u_id {
-                    println!("down up");
-                } else if ev.id == l_r_id {
-                    println!("left right");
-                }
+            if ev.state == global_hotkey::HotKeyState::Pressed
+                && let Some(h) = handlers.get(&ev.id)
+            {
+                h();
             }
         }
     });
